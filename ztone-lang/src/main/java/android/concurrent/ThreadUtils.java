4d3d7e3b7d5e853531f11c8b3a5cb1319c12e9e7
z <z@jzg01.com>
2019-11-23 23:34:28 +0800

    ZTONE_LANG

--- ztone-lang/src/main/java/android/concurrent/ThreadUtils.java
+++ ztone-lang/src/main/java/android/concurrent/ThreadUtils.java
@@ -1,30 +1,17 @@
 package android.concurrent;
 
-import android.assist.Assert;
-import android.reflect.TrackingUtils;
-
 /**
  * Created by handy on 17-3-15.
  */
-
+@Deprecated
 public class ThreadUtils {
 
     public static void sleepThread(long time) {
-        try {
-            Thread.sleep(time);
-        } catch (Exception e) {
-            // do nothing
-        }
+        Threads.sleepThread(time);
     }
 
     public static Thread start(Runnable runnable, String threadName) {
-        Thread thread = null;
-        if (runnable != null) {
-            thread = new Thread(TrackingUtils.proxy(runnable), //
-                    Assert.notEmpty(threadName) ? threadName : "THREAD_" + System.currentTimeMillis());
-            thread.start();
-        }
 
-        return thread;
+        return Threads.start(runnable, threadName);
     }
 }
