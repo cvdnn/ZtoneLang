83db3a2c7c389b6a79d751e26ca8edaf1879742f
liuzheng <r_miyagi@qq.com>
2019-11-26 15:44:03 +0800

    ZTONE_LANG

--- ztone-lang/src/main/java/android/collection/ChokePoint.java
+++ ztone-lang/src/main/java/android/collection/ChokePoint.java
@@ -1,10 +1,27 @@
 package android.collection;
 
+import android.log.Log;
+
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public final class ChokePoint<E> extends LinkedBlockingQueue<E> {
 
     public ChokePoint() {
         super(1);
+    }
+
+
+    @Override
+    public E poll(long timeout, TimeUnit unit) {
+        E e = null;
+
+        try {
+            e = super.poll(timeout, unit);
+        } catch (Exception exc) {
+            Log.e(exc);
+        }
+
+        return e;
     }
 }
