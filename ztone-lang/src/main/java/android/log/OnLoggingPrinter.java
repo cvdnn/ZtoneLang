98db96bdc274f417223952d9d9c1d82d11d19cce
liuzheng <r_miyagi@qq.com>
2020-05-16 17:26:10 +0800

    ZTONE_LANG

--- ztone-lang/src/main/java/android/log/OnLoggingPrinter.java
+++ ztone-lang/src/main/java/android/log/OnLoggingPrinter.java
@@ -6,5 +6,5 @@ package android.log;
 
 public interface OnLoggingPrinter {
 
-    void print(long t, String tag, String message);
+    void print(long t, int priority, String tag, String message);
 }
